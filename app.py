87fb796a7d8490a67f4e067325b7893a6450aa8d
harsha-mangena <harsha.mangena@gmail.com>
2023-09-18 15:05:49 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# Import necessary library
+import streamlit as st
+
+def main():
+    # Display the error message
+    st.title("Error !404")
+    st.subheader("Developer not available")
+
+if __name__ == "__main__":
+    main()
